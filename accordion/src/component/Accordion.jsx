b0b1b34f2b1d02646c709f791b7f903eaec9841d
Your Name <rindepixels@gmail.com>
2024-07-04 12:37:28 +0100

    Implemeted final functionality

--- accordion/src/component/Accordion.jsx
+++ accordion/src/component/Accordion.jsx
@@ -1,26 +1,25 @@
-//Single Selection
-
 import { useState } from "react";
 import data from "./data";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
 
-  function handleSingleSelection(getCurrentId) {
+  function handleSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   }
+
   return (
     <>
-      <div className="flex justify-center items-center h-[100vh] w-[100vw">
+      <div className="flex justify-center items-center h-[100vh] w-[100vw]">
         <div className="w-[500px]">
           {data && data.length > 0 ? (
             data.map((dataItem) => (
               <div
-                className="bg-orange-500 mb-[10px] pt-3 pb-3 pr-5 pl-5"
+                className="bg-orange-800 mb-[10px] pt-3 pb-3 pr-5 pl-5"
                 key={dataItem.id}>
                 <div
-                  onClick={() => handleSingleSelection(dataItem.id)}
-                  className="text-white flex justify-between items-center cursor-pointer">
+                  onClick={() => handleSelection(dataItem.id)}
+                  className="text-white flex justify-between items-center cursor-pointer font-bold">
                   <h3>{dataItem.question}</h3>
                   <span>+</span>
                 </div>
